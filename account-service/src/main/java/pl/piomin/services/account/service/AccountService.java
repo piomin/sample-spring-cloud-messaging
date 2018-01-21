766df8c68131de0f2b3c94a053d590fdd4a185d8
Piotr Mińkowski <piotr_minkowski@wp.pl>
2018-01-21 16:17:20 +0100

    add processing method

--- account-service/src/main/java/pl/piomin/services/account/service/AccountService.java
+++ account-service/src/main/java/pl/piomin/services/account/service/AccountService.java
@@ -1,13 +1,22 @@
 package pl.piomin.services.account.service;
 
+import java.util.List;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+import pl.piomin.services.account.client.ProductClient;
+import pl.piomin.services.account.messaging.OrderSender;
+import pl.piomin.services.account.model.Account;
 import pl.piomin.services.account.model.Order;
+import pl.piomin.services.account.model.OrderStatus;
+import pl.piomin.services.account.model.Product;
+import pl.piomin.services.account.repository.AccountRepository;
 
 @Service
 public class AccountService {
@@ -16,8 +25,29 @@ public class AccountService {
 	
 	private ObjectMapper mapper = new ObjectMapper();
 	
-	public void process(Order order) throws JsonProcessingException {
+	@Autowired
+	AccountRepository accountRepository;
+	@Autowired
+	ProductClient productClient;
+	@Autowired
+	OrderSender orderSender;
+	
+	public void process(final Order order) throws JsonProcessingException {
 		LOGGER.info("Order processed: {}", mapper.writeValueAsString(order));
+		List<Account> accounts =  accountRepository.findByCustomer(order.getCustomerId());
+		Account account = accounts.get(0);
+		LOGGER.info("Account found: {}", mapper.writeValueAsString(account));
+		List<Product> products = productClient.findByIds(order.getProductIds());
+		LOGGER.info("Products found: {}", mapper.writeValueAsString(products));
+		products.forEach(p -> order.setPrice(order.getPrice() + p.getPrice()));
+		if (order.getPrice() <= account.getBalance()) {
+			order.setStatus(OrderStatus.ACCEPTED);
+			account.setBalance(account.getBalance() - order.getPrice());
+		} else {
+			order.setStatus(OrderStatus.REJECTED);
+		}
+		orderSender.send(order);
+		LOGGER.info("Order response sent: {}", mapper.writeValueAsString(order));
 	}
 	
 }
